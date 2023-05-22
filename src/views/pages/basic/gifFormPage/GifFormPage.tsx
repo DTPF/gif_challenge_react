1739109e60201c93334e97a85bf20852f04db792
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-23 00:06:01 +0200

    feat(gif): create form page

---
+++ src/views/pages/basic/gifFormPage/GifFormPage.tsx
@@ -0,0 +1,14 @@
+import HelmetSEO from "src/views/utils/HelmetSEO";
+
+const GifFormPage = () => {
+  return (
+    <HelmetSEO
+      title='Gif Form | DaGif'
+      description='DaGif Gif Form Page'
+    >
+      Gif Form DaGif
+    </HelmetSEO>
+  )
+}
+
+export default GifFormPage;
