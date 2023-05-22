a4be79dd1427fde1f9e3a889e59ce0fc48613d89
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-22 23:32:15 +0200

    feat(errorPage): create component

---
+++ src/views/pages/errors/Error.tsx
@@ -0,0 +1,12 @@
+import HelmetSEO from "src/views/utils/HelmetSEO"
+
+export default function Error() {
+  return (
+    <HelmetSEO
+      title='Error 404 | Audity'
+      description='Audity Error Page'
+    >
+      <div>Error 404</div>
+    </HelmetSEO>
+  )
+}
