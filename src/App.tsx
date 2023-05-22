562c37a9690a0935472ccf2b8b9c4ab53fa702ce
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-22 23:50:35 +0200

    feat(app): add Auth0, cloudinary, and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RouterProvider } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react';
+import UserProvider from './context/user/UserProvider'
+import GifProvider from './context/gif/GifProvider';
+import router from './router/router';
+import { isLocalhost } from './utils/isLocalhost';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN as string}
+      clientId={
+        isLocalhost
+          ? import.meta.env.VITE_DEVELOPMENT_AUTH0_CLIENT_ID as string
+          : import.meta.env.VITE_PRODUCTION_AUTH0_CLIENT_ID as string
+      }
+      authorizationParams={{
+        redirect_uri: window.location.origin + '/',
+        audience: isLocalhost
+          ? import.meta.env.VITE_AUTH0_AUDIENCE_DEVELOPMENT
+          : import.meta.env.VITE_AUTH0_AUDIENCE_PRODUCTION
+      }}
+    >
+      <UserProvider>
+        <GifProvider>
+          <RouterProvider
+            router={router}
+            fallbackElement={<></>}
+          />
+        </GifProvider>
+      </UserProvider>
+    </Auth0Provider>
   )
 }
-
-export default App
