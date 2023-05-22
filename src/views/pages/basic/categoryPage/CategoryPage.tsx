fd11ec1608456d59e5869405b35266536fd172cb
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-22 23:35:55 +0200

    feat(categories): create page

---
+++ src/views/pages/basic/categoryPage/CategoryPage.tsx
@@ -0,0 +1,14 @@
+import HelmetSEO from "src/views/utils/HelmetSEO";
+
+const CategoriesPage = () => {
+  return (
+    <HelmetSEO
+      title='Categories | DaGif'
+      description='DaGif Categories Page'
+    >
+      Categories DaGif
+    </HelmetSEO>
+  )
+}
+
+export default CategoriesPage;
