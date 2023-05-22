7622ae20c9c49dec106a53da7d66f2f06204f1b7
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-22 23:33:36 +0200

    feat(profile): create page

---
+++ src/views/pages/basic/profilePage/ProfilePage.tsx
@@ -0,0 +1,14 @@
+import HelmetSEO from "src/views/utils/HelmetSEO";
+
+const ProfilePage = () => {
+  return (
+    <HelmetSEO
+      title='Profile | DaGif'
+      description='DaGif Profile Page'
+    >
+      Profile DaGif
+    </HelmetSEO>
+  )
+}
+
+export default ProfilePage;
