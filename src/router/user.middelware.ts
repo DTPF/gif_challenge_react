90548d5e90a732c3a15e106d830430e3216083ff
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-23 22:42:50 +0200

    feat(middleware): create protected route for gif

--- src/router/user.middelware.ts
+++ src/router/user.middelware.ts
@@ -1,7 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useAuth0 } from "@auth0/auth0-react";
-import { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import UserContext from "../context/user/UserContext";
+import { Gif } from "src/interfaces/gif";
+import { message } from "antd";
 
 export const ProtectedUser = ({ children }: any) => {
   const { isAuthenticated } = useAuth0()
@@ -12,8 +15,32 @@ export const ProtectedUser = ({ children }: any) => {
     if (!isAuthenticated) {
       navigate('/')
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dbUser]);
+
+  return children;
+};
+
+export const ProtectedGif = ({ children }: any) => {
+  const { gifId } = useParams()
+  const { dbUser } = useContext(UserContext)
+  const navigate = useNavigate()
+  const [isOwnGif, setIsOwnGif] = useState(false)
+
+  useEffect(() => {
+    try {
+      dbUser.gifs.forEach((gif: Gif) => {
+        if (gif._id.toString() === gifId) {
+          setIsOwnGif(true)
+        }
+      });
+
+    } finally {
+      if (!isOwnGif) {
+        message.warning('Unathorized')
+        navigate('/')
+      }
+    }
+  }, [dbUser, gifId]);
 
   return children;
 };
